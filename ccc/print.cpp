9145631f7556f866f7b4f238bcaa7a9f60600a76
chaoticgd <chaoticgd@protonmail.com>
2021-06-15 00:11:03 +0100

    Add the worst JSON writer to ever exist

--- ccc/print.cpp
+++ ccc/print.cpp
@@ -2,7 +2,56 @@
 
 static void indent(FILE* output, s32 depth);
 
-void print_ast_begin(FILE* output) {
+static void print_json_ast_node(FILE* output, const AstNode& node, s32 depth);
+
+void print_ast(FILE* output, const std::vector<AstNode>& ast_nodes, OutputLanguage language, bool verbose) {
+	switch(language) {
+		case OutputLanguage::CPP: {
+			print_c_forward_declarations(stdout, ast_nodes);
+			print_c_ast_begin(stdout);
+			bool last_node_is_not_typedef = true;
+			for(const AstNode& node : ast_nodes) {
+				bool node_is_not_typedef = node.descriptor != AstNodeDescriptor::TYPEDEF;
+				if(node_is_not_typedef || last_node_is_not_typedef) {
+					printf("\n");
+				}
+				last_node_is_not_typedef = node_is_not_typedef;
+				
+				assert(node.symbol);
+				if(verbose) {
+					printf("// %s\n", node.name.c_str());
+				}
+				if(node.conflicting_types) {
+					printf("// warning: multiple differing types with the same name, only one recovered\n");
+				}
+				if(verbose) {
+					printf("// symbol:\n");
+					printf("//   %s\n", node.symbol->raw.c_str());
+					printf("// used by:\n");
+					for(const std::string& source_file : node.source_files) {
+						printf("//   %s\n", source_file.c_str());
+					}
+				}
+				print_c_ast_node(stdout, node, 0, 0);
+			}
+			break;
+		}
+		case OutputLanguage::JSON: {
+			fprintf(output, "[\n");
+			for(size_t i = 0; i < ast_nodes.size(); i++) {
+				bool is_last = i == ast_nodes.size() - 1;
+				print_json_ast_node(output, ast_nodes[i], 1);
+				if(!is_last) {
+					fprintf(output, ",");
+				}
+				fprintf(output, "\n");
+			}
+			fprintf(output, "]\n");
+		}
+	}
+}
+
+void print_c_ast_begin(FILE* output) {
 	printf("\n");
 	fprintf(output, "struct ccc_int128 {\n");
 	fprintf(output, "\tlong int lo;\n");
@@ -10,7 +59,7 @@ void print_ast_begin(FILE* output) {
 	fprintf(output, "};\n");
 }
 
-void print_forward_declarations(FILE* output, const std::vector<AstNode>& ast_nodes) {
+void print_c_forward_declarations(FILE* output, const std::vector<AstNode>& ast_nodes) {
 	for(const AstNode& node : ast_nodes) {
 		bool print = true;
 		switch(node.descriptor) {
@@ -26,7 +75,7 @@ void print_forward_declarations(FILE* output, const std::vector<AstNode>& ast_no
 	}
 }
 
-void print_ast_node(FILE* output, const AstNode& node, s32 depth, s32 absolute_parent_offset) {
+void print_c_ast_node(FILE* output, const AstNode& node, s32 depth, s32 absolute_parent_offset) {
 	indent(output, depth);
 	if(node.is_static) {
 		fprintf(output, "static ");
@@ -79,7 +128,7 @@ void print_ast_node(FILE* output, const AstNode& node, s32 depth, s32 absolute_p
 			}
 			fprintf(output, " {\n");
 			for(const AstNode& child : node.struct_or_union.fields) {
-				print_ast_node(output, child, depth + 1, absolute_parent_offset + node.offset);
+				print_c_ast_node(output, child, depth + 1, absolute_parent_offset + node.offset);
 			}
 			indent(output, depth);
 			printf("}");
@@ -98,6 +147,67 @@ void print_ast_node(FILE* output, const AstNode& node, s32 depth, s32 absolute_p
 		fprintf(output, "[%d]", index);
 	}
 	fprintf(output, ";\n");
+}
+
+static void print_json_ast_node(FILE* output, const AstNode& node, s32 depth) {
+	indent(output, depth); fprintf(output, "{\n");
+	if(node.is_static) {
+		indent(output, depth + 1); fprintf(output, "\"is_static\": true,\n");
+	} else {
+		indent(output, depth + 1); fprintf(output, "\"is_static\": false,\n");
+		indent(output, depth + 1); fprintf(output, "\"offset\": %d,\n", node.offset);
+		indent(output, depth + 1); fprintf(output, "\"size\": %d,\n", node.size);
+	}
+	indent(output, depth + 1); fprintf(output, "\"name\": \"%s\",\n", node.name.c_str());
+	switch(node.descriptor) {
+		case AstNodeDescriptor::LEAF: {
+			indent(output, depth + 1); fprintf(output, "\"type\": \"leaf\",\n");
+			indent(output, depth + 1); fprintf(output, "\"type_name\": \"%s\",\n", node.leaf.type_name.c_str());
+			break;
+		}
+		case AstNodeDescriptor::ENUM: {
+			indent(output, depth + 1); fprintf(output, "\"type\": \"leaf\",\n");
+			break;
+		}
+		case AstNodeDescriptor::STRUCT:
+		case AstNodeDescriptor::UNION: {
+			bool is_struct = node.descriptor == AstNodeDescriptor::STRUCT;
+			indent(output, depth + 1); fprintf(output, "\"type\": \"%s\",\n", is_struct ? "struct" : "union");
+			indent(output, depth + 1); fprintf(output, "\"base_classes\": [\n");
+			const auto& base_classes = node.struct_or_union.base_classes;
+			for(size_t i = 0; i < base_classes.size(); i++) {
+				bool is_last = i == base_classes.size() - 1;
+				const AstBaseClass& base_class = base_classes[i];
+				indent(output, depth + 2); fprintf(output, "{\n");
+				indent(output, depth + 3); fprintf(output, "\"offset\": %d,\n", base_class.offset);
+				indent(output, depth + 3); fprintf(output, "\"type_name\": \"%s\"\n", base_class.type_name.c_str());
+				indent(output, depth + 2); fprintf(output, "}%s\n", is_last ? "" : ",");
+			}
+			indent(output, depth + 1); fprintf(output, "],\n");
+			indent(output, depth + 1); fprintf(output, "\"fields\": [\n");
+			for(size_t i = 0; i < node.struct_or_union.fields.size(); i++) {
+				bool is_last = i == node.struct_or_union.fields.size() - 1;
+				const AstNode& child_node = node.struct_or_union.fields[i];
+				print_json_ast_node(output, child_node, depth + 2);
+				if(!is_last) {
+					fprintf(output, ",");
+				}
+				fprintf(output, "\n");
+			}
+			indent(output, depth + 1); fprintf(output, "],\n");
+			break;
+		}
+		case AstNodeDescriptor::TYPEDEF: {
+			indent(output, depth + 1); fprintf(output, "\"type\": \"typedef\",\n");
+			indent(output, depth + 1); fprintf(output, "\"type_name\": \"%s\",\n", node.typedef_type.type_name.c_str());
+			break;
+		}
+	}
+	if(node.symbol) {
+		indent(output, depth + 1); fprintf(output, "\"stabs_symbol\": \"%s\",\n", node.symbol->raw.c_str());
+	}
+	indent(output, depth + 1); fprintf(output, "\"conflicting_types\": %s\n", node.conflicting_types ? "true" : "false");
+	indent(output, depth); fprintf(output, "}");
 }
 
 static void indent(FILE* output, s32 depth) {
