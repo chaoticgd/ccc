3a9480479c8bc495b5cfc835e88a243ac1fc297a
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2022-12-30 19:34:25 +0000

    Clean up how mdebug HDRR is printed out

--- ccc/mdebug.cpp
+++ ccc/mdebug.cpp
@@ -8,25 +8,25 @@ packed_struct(SymbolicHeader,
 	/* 0x04 */ s32 line_number_count;
 	/* 0x08 */ s32 line_numbers_size_bytes;
 	/* 0x0c */ s32 line_numbers_offset;
-	/* 0x10 */ s32 dense_numbers_max_index;
+	/* 0x10 */ s32 dense_numbers_count;
 	/* 0x14 */ s32 dense_numbers_offset;
 	/* 0x18 */ s32 procedure_descriptor_count;
 	/* 0x1c */ s32 procedure_descriptors_offset;
 	/* 0x20 */ s32 local_symbol_count;
 	/* 0x24 */ s32 local_symbols_offset;
-	/* 0x28 */ s32 optimization_symbols_max_index;
+	/* 0x28 */ s32 optimization_symbols_count;
 	/* 0x2c */ s32 optimization_symbols_offset;
 	/* 0x30 */ s32 auxiliary_symbol_count;
 	/* 0x34 */ s32 auxiliary_symbols_offset;
-	/* 0x38 */ s32 local_strings_max_index;
+	/* 0x38 */ s32 local_strings_size_bytes;
 	/* 0x3c */ s32 local_strings_offset;
-	/* 0x40 */ s32 external_strings_max_index;
+	/* 0x40 */ s32 external_strings_size_bytes;
 	/* 0x44 */ s32 external_strings_offset;
 	/* 0x48 */ s32 file_descriptor_count;
 	/* 0x4c */ s32 file_descriptors_offset;
 	/* 0x50 */ s32 relative_file_descriptor_count;
 	/* 0x54 */ s32 relative_file_descriptors_offset;
-	/* 0x58 */ s32 external_symbols_max_index;
+	/* 0x58 */ s32 external_symbols_count;
 	/* 0x5c */ s32 external_symbols_offset;
 )
 
@@ -74,15 +74,15 @@ packed_struct(ProcedureDescriptor,
 	/* 0x30 */ s32 cb_line_offset;
 )
 
-packed_struct(SymbolEntry,
-	u32 iss;
-	s32 value;
-	u32 st : 6;
-	u32 sc : 5;
-	u32 reserved : 1;
-	u32 index : 20;
+packed_struct(LocalSymbol,
+	/* 0x00 */ u32 iss;
+	/* 0x04 */ s32 value;
+	/* 0x08:00 */ u32 st : 6;
+	/* 0x08:06 */ u32 sc : 5;
+	/* 0x08:11 */ u32 reserved : 1;
+	/* 0x08:12 */ u32 index : 20;
 )
-static_assert(sizeof(SymbolEntry) == 0xc);
+static_assert(sizeof(LocalSymbol) == 0xc);
 
 SymbolTable parse_symbol_table(const Module& module, const ModuleSection& section) {
 	SymbolTable symbol_table;
@@ -118,8 +118,8 @@ SymbolTable parse_symbol_table(const Module& module, const ModuleSection& sectio
 		
 		// Read symbols.
 		for(s64 j = 0; j < fd_header.symbol_count; j++) {
-			u64 sym_offset = hdrr.local_symbols_offset + (fd_header.isym_base + j) * sizeof(SymbolEntry);
-			const auto& sym_entry = get_packed<SymbolEntry>(module.image, sym_offset, "local symbol");
+			u64 sym_offset = hdrr.local_symbols_offset + (fd_header.isym_base + j) * sizeof(LocalSymbol);
+			const auto& sym_entry = get_packed<LocalSymbol>(module.image, sym_offset, "local symbol");
 			Symbol& sym = fd.symbols.emplace_back();
 			sym.string = get_string(module.image, hdrr.local_strings_offset + fd_header.strings_offset + sym_entry.iss);
 			sym.value = sym_entry.value;
@@ -152,8 +152,8 @@ SymbolTable parse_symbol_table(const Module& module, const ModuleSection& sectio
 		//	u64 pd_offset = hdrr.cb_pd_offset + (fd_header.ipd_first + j) * sizeof(ProcedureDescriptor);
 		//	auto pd_entry = get_packed<ProcedureDescriptor>(module.image, pd_offset, "procedure descriptor");
 		//	
-		//	u64 sym_offset = hdrr.cb_sym_offset + (fd_header.isym_base + pd_entry.isym) * sizeof(SymbolEntry);
-		//	const auto& sym_entry = get_packed<SymbolEntry>(module.image, sym_offset, "local symbol");
+		//	u64 sym_offset = hdrr.cb_sym_offset + (fd_header.isym_base + pd_entry.isym) * sizeof(LocalSymbol);
+		//	const auto& sym_entry = get_packed<LocalSymbol>(module.image, sym_offset, "local symbol");
 		//	
 		//	SymProcedureDescriptor& pd = fd.procedures.emplace_back();
 		//	pd.name = get_string(module.image, hdrr.strings_base_offset + fd_header.strings_offset + sym_entry.iss);
@@ -170,30 +170,49 @@ void print_headers(FILE* dest, const SymbolTable& symbol_table) {
 	const SymbolicHeader& hdrr = *symbol_table.header;
 	fprintf(dest, "SYMBOLIC HEADER: magic=%hx vstamp=%hx\n", hdrr.magic, hdrr.version_stamp);
 	fprintf(dest, "\n");
-	fprintf(dest, "                              Count     Offset    Size      Max Index\n");
-	fprintf(dest, "                              -----     ------    ----      ---------\n");
-	fprintf(dest, "  Line Numbers                %-8x  %-8x  %-8x  -\n",
-		hdrr.line_number_count, hdrr.line_numbers_offset, hdrr.line_numbers_size_bytes);
-	fprintf(dest, "  Dense Numbers               -         %-8x  -         %-8x\n",
-		hdrr.dense_numbers_offset, hdrr.dense_numbers_max_index);
-	fprintf(dest, "  Procedure Descriptors       %-8x  %-8x  -         -\n",
-		hdrr.procedure_descriptor_count, hdrr.procedure_descriptors_offset);
-	fprintf(dest, "  Local Symbols               %-8x  %-8x  -         -\n",
-		hdrr.local_symbol_count, hdrr.local_symbols_offset);
-	fprintf(dest, "  Optimization Symbols        -         %-8x  -         %-8x\n",
-		hdrr.optimization_symbols_offset, hdrr.optimization_symbols_max_index);
-	fprintf(dest, "  Auxiliary Symbols           %-8x  %-8x  -         -\n",
-		hdrr.auxiliary_symbol_count, hdrr.auxiliary_symbols_offset);
-	fprintf(dest, "  Local Strings               -         %-8x  -         %-8x\n",
-		hdrr.local_strings_offset, hdrr.local_strings_max_index);
-	fprintf(dest, "  External Strings            -         %-8x  -         %-8x\n",
-		hdrr.external_strings_offset, hdrr.external_strings_max_index);
-	fprintf(dest, "  File Descriptors            %-8x  %-8x  -         -\n",
-		hdrr.file_descriptor_count, hdrr.file_descriptors_offset);
-	fprintf(dest, "  Relative Files Descriptors  %-8x  %-8x  -         -\n",
-		hdrr.relative_file_descriptor_count, hdrr.relative_file_descriptors_offset);
-	fprintf(dest, "  External Symbols            -         %-8x  -         %-8x\n",
-		hdrr.external_symbols_offset, hdrr.external_symbols_max_index);
+	fprintf(dest, "                              Offset              Size (Bytes)        Count\n");
+	fprintf(dest, "                              ------              ------------        -----\n");
+	fprintf(dest, "  Line Numbers                0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.line_numbers_offset,
+		hdrr.line_numbers_size_bytes,
+		hdrr.line_number_count);
+	fprintf(dest, "  Dense Numbers               0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.dense_numbers_offset,
+		hdrr.dense_numbers_count * 8,
+		hdrr.dense_numbers_count);
+	fprintf(dest, "  Procedure Descriptors       0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.procedure_descriptors_offset,
+		hdrr.procedure_descriptor_count * (s32) sizeof(ProcedureDescriptor),
+		hdrr.procedure_descriptor_count);
+	fprintf(dest, "  Local Symbols               0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.local_symbols_offset,
+		hdrr.local_symbol_count * (s32) sizeof(LocalSymbol),
+		hdrr.local_symbol_count);
+	fprintf(dest, "  Optimization Symbols        0x%-8x          "  "-                   "  "%-8d\n",
+		hdrr.optimization_symbols_offset,
+		hdrr.optimization_symbols_count);
+	fprintf(dest, "  Auxiliary Symbols           0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.auxiliary_symbols_offset,
+		hdrr.auxiliary_symbol_count * 4,
+		hdrr.auxiliary_symbol_count);
+	fprintf(dest, "  Local Strings               0x%-8x          "  "-                   "  "%-8d\n",
+		hdrr.local_strings_offset,
+		hdrr.local_strings_size_bytes);
+	fprintf(dest, "  External Strings            0x%-8x          "  "-                   "  "%-8d\n",
+		hdrr.external_strings_offset,
+		hdrr.external_strings_size_bytes);
+	fprintf(dest, "  File Descriptors            0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.file_descriptors_offset,
+		hdrr.file_descriptor_count * (s32) sizeof(FileDescriptor),
+		hdrr.file_descriptor_count);
+	fprintf(dest, "  Relative Files Descriptors  0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.relative_file_descriptors_offset,
+		hdrr.relative_file_descriptor_count * 4,
+		hdrr.relative_file_descriptor_count);
+	fprintf(dest, "  External Symbols            0x%-8x          "  "0x%-8x          "  "%-8d\n",
+		hdrr.external_symbols_offset,
+		hdrr.external_symbols_count * 16,
+		hdrr.external_symbols_count);
 }
 
 const char* symbol_type(SymbolType type) {
