fd81fd2119401bb6123864ab1d4b6433ade84fb5
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-01-24 23:21:10 +0000

    Try once again to fix the windows issue

--- cmake/version_finder.cmake
+++ cmake/version_finder.cmake
@@ -10,14 +10,13 @@ const char* git_tag() {
 ]])
 set(GIT_TAG_H_PATH "${CMAKE_BINARY_DIR}/git_tag.h")
 set(GIT_TAG_FORMAT [[\"%\(refname:strip=2\)\"]])
-set(GIT_TAG_COMMAND ${GIT_EXECUTABLE} tag --points-at HEAD --format ${GIT_TAG_FORMAT} > "${GIT_TAG_H_PATH}")
 file(WRITE "${GIT_TAG_CPP_PATH}" "${GIT_TAG_CPP}")
 file(WRITE "${GIT_TAG_H_PATH}" "")
 find_package(Git QUIET)
 if(GIT_FOUND AND EXISTS "${CMAKE_SOURCE_DIR}/.git")
 	message(STATUS "Found git")
 	add_custom_command(
-		COMMAND ${GIT_TAG_COMMAND}
+		COMMAND ${GIT_EXECUTABLE} tag --points-at HEAD --format "${GIT_TAG_FORMAT}" > "${GIT_TAG_H_PATH}"
 		DEPENDS "${CMAKE_SOURCE_DIR}/*"
 		OUTPUT "${GIT_TAG_H_PATH}"
 	)
