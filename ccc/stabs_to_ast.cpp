3c4baf5262e62f0091482a35ad0cf0e1564950d4
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-10-29 16:52:54 +0000

    Fix incorrect stringf statement

--- ccc/stabs_to_ast.cpp
+++ ccc/stabs_to_ast.cpp
@@ -84,7 +84,7 @@ Result<std::unique_ptr<ast::Node>> stabs_type_to_ast(const StabsType& type, cons
 		if(type.anonymous || stabs_type == state.stabs_types->end()) {
 			auto type_name = std::make_unique<ast::TypeName>();
 			type_name->source = ast::TypeNameSource::ERROR;
-			type_name->type_name = stringf("CCC_BADTYPELOOKUP(%d)", type.type_number);
+			type_name->type_name = stringf("CCC_BADTYPELOOKUP(%d,%d)", type.type_number.file, type.type_number.type);
 			return std::unique_ptr<ast::Node>(std::move(type_name));
 		}
 		return stabs_type_to_ast(*stabs_type->second, state, abs_parent_offset_bytes, depth + 1, substitute_type_name, force_substitute);
