0569e803a3ac806e57bec6d79883dd9d7645b799
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2023-11-30 07:51:14 +0000

    Reimplement the main test

--- src/tests.cpp
+++ src/tests.cpp
@@ -8,7 +8,7 @@
 
 using namespace ccc;
 
-static void main_test(const fs::path& input_directory);
+static int main_test(const fs::path& input_directory);
 
 int main(int argc, char** argv) {
 	testing::InitGoogleTest(&argc, argv);
@@ -17,22 +17,36 @@ int main(int argc, char** argv) {
 		return result;
 	}
 	
-	if(argc == 2) {
-		main_test(std::string(argv[1]));
+	if(argc != 2) {
+		return 1;
 	}
+	
+	return main_test(std::string(argv[1]));
 }
 
-static void main_test(const fs::path& input_directory) {
+static int main_test(const fs::path& input_directory) {
 	CCC_CHECK_FATAL(fs::is_directory(input_directory), "Input path is not a directory.");
 	
 	for(auto entry : fs::recursive_directory_iterator(input_directory)) {
 		if(entry.is_regular_file()) {
 			printf("%s ", entry.path().string().c_str());
 			
-			Result<std::vector<u8>> file = platform::read_binary_file(entry.path());
-			CCC_EXIT_IF_ERROR(file);
+			Result<std::vector<u8>> image = platform::read_binary_file(entry.path());
+			CCC_EXIT_IF_ERROR(image);
+			
+			Result<SymbolFile> symbol_file = parse_symbol_file(*image);
+			if(symbol_file.success()) {
+				SymbolDatabase database;
+				SymbolTableConfig importer_config;
+				Result<SymbolSourceHandle> handle = import_symbol_table(database, *symbol_file, importer_config);
+				CCC_EXIT_IF_ERROR(handle);
+			} else {
+				printf("%s", symbol_file.error().message.c_str());
+			}
 			
 			printf("\n");
 		}
 	}
+	
+	return 0;
 }
