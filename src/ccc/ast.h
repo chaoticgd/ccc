ca51eab1b2aa10ea16f337cb41c4a0c89ce56746
chaoticgd <43898262+chaoticgd@users.noreply.github.com>
2024-03-19 21:09:14 +0000

    Make Node::flatten_fields return true if all fields were enumerated

--- src/ccc/ast.h
+++ src/ccc/ast.h
@@ -180,8 +180,10 @@ struct StructOrUnion : Node {
 	static const constexpr NodeDescriptor DESCRIPTOR = STRUCT_OR_UNION;
 	
 	// Generate a flat list of all the fields in this class as well as all the
-	// base classes recursively, but only until max_fields is reached.
-	void flatten_fields(
+	// base classes recursively, but only until max_fields is reached. Returns
+	// true if all fields were returned, or false if the max_fields or max_depth
+	// limits were exceeded.
+	bool flatten_fields(
 		std::vector<std::pair<const Node*, const DataType*>>& output,
 		size_t max_fields,
 		size_t max_depth,
